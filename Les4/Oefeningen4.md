6b446d4ab77adb1f63f70eb93e1d819db0665d3f
pietervdvn <pietervdvn@gmail.com>
2015-03-18 21:14:47 +0100

    State

--- Les4/Oefeningen4.md
+++ Les4/Oefeningen4.md
@@ -27,6 +27,8 @@ Zie quickcheck.hs. Welke eigenschappen zijn onjuist?
 State
 -----
 
+Don't panic als nog niet alles duidelijk is, volgende week gaan we hiermee verder.
+
 Implementeer zelf State. Schrijf, met behulp van State, een algoritme dat de som neemt van een lijst.
 Echter, iedere keer dat je een twee tegenkomt, tel je 2 op bij de som en telt het volgende getal dubbel.
 
--- Les4/State.hs
+++ Les4/State.hs
@@ -1,5 +1,6 @@
 module State where
 
+import Prelude hiding (return,sum)
 
 data State s a	= State {runstate	:: s -> (s,a)}
 
@@ -9,9 +10,37 @@ put s	= State (\_ -> (s,()))
 get	:: State s s
 get	= State (\s -> (s,s))
 
+return	:: a -> State s a
+return a=  State (\s -> (s,a))
+
 andThen	:: State s a -> (a -> State s b) -> State s b
 andThen action	a2actionb
 	= State $ \beginState ->
 		let 	(midState, a)	= runstate action beginState
 			(endState, b)	= runstate (a2actionb a) midState in
 			(endState, b)
+
+
+andThen'	:: State s a -> State s b -> State s b
+andThen' action1 action2
+	= action1 `andThen` (\_ -> action2)
+
+sum	:: [Int] -> Int
+sum is	=  snd $ runstate sum' is
+
+sum'	:: State [Int] Int
+sum'	=  get `andThen` (\is ->
+		if null is then return 0 else
+		let i	= head is in
+		put (tail is) `andThen'`
+		sum' `andThen` (\recSum -> return $ i + recSum)
+		)
+
+
+sum'	= do	is	<- get
+		if null is then
+			return 0
+		else do
+			put (tail is)
+			recursiveSum	<- sum'
+			return $ recursiveSum + head is
